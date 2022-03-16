ee01cc8f1f5f4e59cde63f8ee16890a0e94cbc2a
JarrodM-M <85252030+JarrodM-M@users.noreply.github.com>
2022-03-16 17:09:48 -0230

    background grid and tile added

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,54 +1,24 @@
 import React from 'react';
+import Tile from './Tile'
 import './Gameboard.css';
 
 const verticalAxis = ['0', '1', '2', '3', '4', '5', '6', '7', '8']; 
 const horizontalAxis = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
 
-/*const boardArray = horizontalAxis.map((x) => (
-    verticalAxis.map((y) => (
-        board.push([y + x])))
-))*/
 
 export default function Gameboard() {
     const board = horizontalAxis.map((x) => (
         verticalAxis.map((y) => (
-            <div className='tile' key= {x+y}>
-                [{x + y}]
+            <div key = {x + y}>
+                <Tile />
             </div>))
     ));
 
-    
 
-    
-    
-    /*for (let j = verticalAxis.length-1; j >= 0; j--){
-        for(let i = 0; i < horizontalAxis.length; i++){
-            board.push(
-            <div className='tile'>[{horizontalAxis[i]}{verticalAxis[j]}]</div>)
-        }
-    }*/
-
-    
-    
     return(
-       
-       /* <div id="gameboard">
-            {horizontalAxis.map((x) => (
-                verticalAxis.map((y) => (
-                    <div className='tile' key= {y+x}>
-                        [{y + x}]
-                    </div>
-                ))
-            ))}
-            {console.log()}
-        </div>*/
-        <div id ='gameboard'>
-            {board};
-            {console.log(board)}
-        </div>
-        
-        
-        
+            <div id ='gameboard'>
+                {board};
+                {console.log(board)}
+            </div>
     );
 }
-/*commit test */
---
+++ src/components/Tile.js
@@ -0,0 +1,13 @@
+import './Tile.css';
+
+
+/*<img src="G:\Coding\pokerdice\src\assets\images\board\00.png"></img>*/
+
+export default function Tile() { 
+    return (
+        <div className='tile' >
+              
+        </div>
+    )};
+
+    
