5123df789f8b8eb2f8fcbecbcb328e31922a0f25
JarrodM-M <jarrod.morey@gmail.com>
2022-03-28 12:22:01 -0230

    5 die roll working
    5 die are rolling and corresponding images are being shown, their values are being pushed into an array

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -9,37 +9,57 @@ import dicepicSix from '../assets/images/die6.png'
 
 
 
-const roll = () => 
-    Math.floor(Math.random() * 6 ) + 1
-
-
-
 export default function DiceFunction(){
-    
-    const [diceOne, setDiceOne] = useState('')
-    const [diceTwo, setDiceTwo] = useState('')
-    const [diceThree, setDiceThree] = useState('')
-    const [diceFour, setDiceFour] = useState('')
-    const [diceFive, setDiceFive] = useState('')
 
-    const handleRoll = () =>{ 
+    const roll = () => Math.floor(Math.random() * 6 ) + 1
+    
+    
+    const [diceOne, setDiceOne] = useState(roll)
+    const [diceTwo, setDiceTwo] = useState(roll)
+    const [diceThree, setDiceThree] = useState(roll)
+    const [diceFour, setDiceFour] = useState(roll)
+    const [diceFive, setDiceFive] = useState(roll)
+
+    const [diceImageOne, setDiceImageOne] = useState(dicepicOne)
+    const [diceImageTwo, setDiceImageTwo] = useState(dicepicOne)
+    const [diceImageThree, setDiceImageThree] = useState(dicepicOne)
+    const [diceImageFour, setDiceImageFour] = useState(dicepicOne)
+    const [diceImageFive, setDiceImageFive] = useState(dicepicOne)
+
+    
+    
+    function handleRoll() {
+
         setDiceOne(roll)
         setDiceTwo(roll)
         setDiceThree(roll)
         setDiceFour(roll)
         setDiceFive(roll)
 
+        setDiceImageOne(imageSelector(diceOne))
+        setDiceImageTwo(imageSelector(diceTwo))
+        setDiceImageThree(imageSelector(diceThree))
+        setDiceImageFour(imageSelector(diceFour))
+        setDiceImageFive(imageSelector(diceFive))
+
+        const diceArray =[diceOne, diceTwo, diceThree, diceFour, diceFive]
+        console.log(diceArray)
     }
+    
 
-    const [diceImageOne, setDiceImageOne] = useState({dicepicOne})
+    function imageSelector (number){
+            if ( number == 1) return dicepicOne
+            if ( number == 2) return dicepicTwo
+            if ( number == 3) return dicepicThree
+            if ( number == 4) return dicepicFour
+            if ( number == 5) return dicepicFive
+            return dicepicSix
+            
+        }
 
-    const imageSelector = () => {
-        setDiceImageOne(()=>{
-            if ( diceOne == 1)
-            return dicepicOne
-        })
+    
 
-    }
+    
 
     return(
         <> 
@@ -50,10 +70,10 @@ export default function DiceFunction(){
             </div>
             <div className='dice-container'>
                 <div className='dice'> <img className= 'dice-image' src={diceImageOne} /> </div>
-                <div className='dice'>{diceTwo}</div>
-                <div className='dice'>{diceThree}</div>
-                <div className='dice'>{diceFour}</div>
-                <div className='dice'>{diceFive}</div>
+                <div className='dice'><img className= 'dice-image' src={diceImageTwo} /></div>
+                <div className='dice'><img className= 'dice-image' src={diceImageThree} /></div>
+                <div className='dice'><img className= 'dice-image' src={diceImageFour} /></div>
+                <div className='dice'><img className= 'dice-image' src={diceImageFive} /></div>
             </div>
         </>
     )
