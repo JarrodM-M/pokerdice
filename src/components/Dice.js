da0f98aca94bc29b8dae683e14a25a4f3ea1afb6
JarrodM-M <jarrod.morey@gmail.com>
2022-04-02 17:33:07 -0230

    Update Dice.js

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -97,7 +97,7 @@ export default function DiceFunction(){
                 <button id='diceButton' onClick={handleRoll} disabled={rollCount >= 3}>
                 roll
                 </button>
-                <button id='diceButton'  onClick= {() => dispatch('reset')} >
+                <button id='diceButton'  onClick= {() =>{dispatch('reset'); toggleOne(isToggledOne ? true : false); toggleTwo(isToggledOne ? false : false); toggleThree(false); toggleFour(false); toggleFive(false);}} >
                 reset
                 </button>
             </div>
