2e48f825325b744629522adcd0d203931e381472
JarrodM-M <85252030+JarrodM-M@users.noreply.github.com>
2022-04-06 19:41:46 -0230

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <div id="app">
 
       {/* <Gameboard /> */}
-      {/* <DiceFunction /> */}
-      <DiceModel /> 
+      <DiceFunction />
+      {/* <DiceModel />  */}
 
     </div>
   );
