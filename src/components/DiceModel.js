e400a2590fbf86376ca60f1cbd101623180f1e3a
JarrodM-M <jarrod.morey@gmail.com>
2022-04-09 20:55:32 -0230

    trying to set position

--- src/components/DiceModel.js
+++ src/components/DiceModel.js
@@ -1,6 +1,7 @@
 import React, { useRef, Suspense, useState } from 'react'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader.js'
+import { useSpring, animated, config } from '@react-spring/three'
 import diceOneTexture from '../assets/images/textures/dice_1.jpeg'
 import diceTwoTexture from '../assets/images/textures/dice_2.jpeg'
 import diceThreeTexture from '../assets/images/textures/dice_3.jpeg'
@@ -10,28 +11,35 @@ import diceSixTexture from '../assets/images/textures/dice_6.jpeg'
 
 function Box() {
 
-    const texture_1 = useLoader(TextureLoader, diceOneTexture)
+    const texture_1 = useLoader(TextureLoader, diceFiveTexture)
     const texture_2 = useLoader(TextureLoader, diceTwoTexture)
     const texture_3 = useLoader(TextureLoader, diceThreeTexture)
     const texture_4 = useLoader(TextureLoader, diceFourTexture)
-    const texture_5 = useLoader(TextureLoader, diceFiveTexture)
+    const texture_5 = useLoader(TextureLoader, diceOneTexture)
     const texture_6 = useLoader(TextureLoader, diceSixTexture)
     const [hovered, hover] = useState(false)
     const [clicked, click] = useState(false)
+    const [position, setPosition] = useState([0,0,0])
 
     const textures = [ texture_1, texture_2, texture_3, texture_4, texture_5, texture_6 ]
     
     const mesh = useRef()
-    useFrame(() => {
-        mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+    // useFrame(() => {
+    //     mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+    // })
+    const {scale} = useSpring({
+        scale: hovered ? 2 : 1.5,
+        config: config.wobbly
     })
+    
     return (
-        <mesh 
+        <animated.mesh 
           ref={mesh}
-          scale={hovered ? 2 : 1.5}
-          onClick={(event) => click(!clicked)}
+          scale={scale}
+          onClick={(event) => click(!clicked), setPosition([3,0,0])}
           onPointerOver={(event) => hover(true)}
           onPointerOut={(event) => hover(false)}
+          position = {position}
         >
             <boxBufferGeometry attach="geometry" />
             {textures.map((texture, index) => 
@@ -42,7 +50,7 @@ function Box() {
                 color={clicked ? 'rgb(127, 103, 143)' : 'white'} 
               />)
             }
-        </mesh>
+        </animated.mesh>
     )
 }
 
