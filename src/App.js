159f8a7bd7202847c1e2dcdf8073c50e7abb8000
JarrodM-M <jarrod.morey@gmail.com>
2022-04-18 16:05:34 -0230

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import './App.css';
-import React from 'react';
-import Gameboard from './components/Gameboard';
-import DiceModel from './components/DiceModel';
-import DiceFunction from './components/Dice';
+import "./App.css";
+import React from "react";
+import Gameboard from "./components/Gameboard";
+import DiceModel from "./components/DiceModel";
+import DiceFunction from "./components/Dice";
 
 function App() {
   return (
     <div id="app">
-
       <Gameboard />
-      {/* <DiceFunction /> */}
-      <div id = 'dice-container'>
-        <DiceModel />
-        <DiceModel />
-        <DiceModel />
-        <DiceModel />
-        <DiceModel />
-      </div>  
+      <DiceFunction />
 
+      <div id="dice-container">
+        {/* <DiceModel />
+        <DiceModel />
+        <DiceModel />
+        <DiceModel />
+        <DiceModel /> */}
+      </div>
     </div>
   );
 }
