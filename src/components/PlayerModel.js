442646f0048a7ada7095fa03d0e5d3d226e0ad0b
JarrodM-M <jarrod.morey@gmail.com>
2022-04-23 16:26:12 -0230

    fontawesomereat for player

--- src/components/PlayerModel.js
+++ src/components/PlayerModel.js
@@ -1,2 +1,10 @@
 // this is where i'll build the model and animation for which current player is selected
-import { fasFaUserAstronaut } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+export default function PlayerModel() {
+  return (
+    <div>
+      <i class="fa-solid fa-user-astronaut">hello</i>
+    </div>
+  );
+}
