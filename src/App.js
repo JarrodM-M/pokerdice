1bcdb3a6e6dc5de33a80b7300ef4730725ef8e42
JarrodM-M <jarrod.morey@gmail.com>
2022-05-02 19:19:17 -0230

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import Gameboard from "./components/Gameboard";
 import DiceFunction from "./components/Dice";
 
 const initialDiceValue = {
-  diceOne: null,
-  diceTwo: null,
-  diceThree: null,
-  diceFour: null,
-  diceFive: null
+  allDice: {
+    diceOne: null,
+    diceTwo: null,
+    diceThree: null,
+    diceFour: null,
+    diceFive: null
+  }
 };
 
 const diceReducer = (state, action) => {
   switch (action.type) {
-    case "setDiceOne":
-      return { ...state, diceOne: action.payload };
+    case "setDice":
+      return {
+        ...state,
+        allDice: {
+          diceOne: action.payload,
+          diceTwo: action.payload,
+          diceThree: action.payload,
+          diceFour: action.payload,
+          diceFive: action.payload
+        }
+      };
     case "setDiceTwo":
       return { ...state, diceTwo: action.payload };
     case "setDiceThree":
@@ -36,20 +47,12 @@ function App() {
   return (
     <div className="app">
       <div className="gamesize">
-        <Gameboard
-          dice={[
-            diceRoll.diceOne,
-            diceRoll.diceTwo,
-            diceRoll.diceThree,
-            diceRoll.diceFour,
-            diceRoll.diceFive
-          ]}
-        />
+        <Gameboard dice={[diceRoll.allDice]} />
       </div>
       <div className="dice">
         <DiceFunction
           resetDice={() => dispatch({ type: "resetAll" })}
-          setDiceOne={x => dispatch({ type: "setDiceOne", payload: x })}
+          setDiceOne={x => dispatch({ type: "setDice", payload: x })}
           diceOne={diceRoll.diceOne}
           setDiceTwo={x => dispatch({ type: "setDiceTwo", payload: x })}
           diceTwo={diceRoll.diceTwo}
