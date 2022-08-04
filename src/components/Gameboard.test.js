924e2f867ac7578b94cfbb30a41cd406cea1cb3b
JarrodM-M <jarrod.morey@gmail.com>
2022-08-04 12:19:17 -0230

    diagonal winning check working from top left to bottom right

--- src/components/Gameboard.test.js
+++ src/components/Gameboard.test.js
@@ -868,6 +868,432 @@ const testBoardY = [
   ]
 ];
 
+const testBoardD = [
+  [
+    {
+      x: "0",
+      y: "0",
+      owner: "blue"
+    },
+    {
+      x: "0",
+      y: "1",
+      owner: "red"
+    },
+    {
+      x: "0",
+      y: "2",
+      owner: "blue"
+    },
+    {
+      x: "0",
+      y: "3",
+      owner: "blue"
+    },
+    {
+      x: "0",
+      y: "4",
+      owner: "red"
+    },
+    {
+      x: "0",
+      y: "5",
+      owner: "blue"
+    },
+    {
+      x: "0",
+      y: "6",
+      owner: "blue"
+    },
+    {
+      x: "0",
+      y: "7",
+      owner: "red"
+    },
+    {
+      x: "0",
+      y: "8",
+      owner: null
+    }
+  ],
+  [
+    {
+      x: "1",
+      y: "0",
+      owner: null
+    },
+    {
+      x: "1",
+      y: "1",
+      owner: "blue"
+    },
+    {
+      x: "1",
+      y: "2",
+      owner: "blue"
+    },
+    {
+      x: "1",
+      y: "3",
+      owner: "red"
+    },
+    {
+      x: "1",
+      y: "4",
+      owner: "red"
+    },
+    {
+      x: "1",
+      y: "5",
+      owner: null
+    },
+    {
+      x: "1",
+      y: "6",
+      owner: "red"
+    },
+    {
+      x: "1",
+      y: "7",
+      owner: "red"
+    },
+    {
+      x: "1",
+      y: "8",
+      owner: "red"
+    }
+  ],
+  [
+    {
+      x: "2",
+      y: "0",
+      owner: "red"
+    },
+    {
+      x: "2",
+      y: "1",
+      owner: "red"
+    },
+    {
+      x: "2",
+      y: "2",
+      owner: "blue"
+    },
+    {
+      x: "2",
+      y: "3",
+      owner: null
+    },
+    {
+      x: "2",
+      y: "4",
+      owner: null
+    },
+    {
+      x: "2",
+      y: "5",
+      owner: null
+    },
+    {
+      x: "2",
+      y: "6",
+      owner: null
+    },
+    {
+      x: "2",
+      y: "7",
+      owner: null
+    },
+    {
+      x: "2",
+      y: "8",
+      owner: null
+    }
+  ],
+  [
+    {
+      x: "3",
+      y: "0",
+      owner: null
+    },
+    {
+      x: "3",
+      y: "1",
+      owner: "red"
+    },
+    {
+      x: "3",
+      y: "2",
+      owner: "blue"
+    },
+    {
+      x: "3",
+      y: "3",
+      owner: "blue"
+    },
+    {
+      x: "3",
+      y: "4",
+      owner: null
+    },
+    {
+      x: "3",
+      y: "5",
+      owner: null
+    },
+    {
+      x: "3",
+      y: "6",
+      owner: null
+    },
+    {
+      x: "3",
+      y: "7",
+      owner: null
+    },
+    {
+      x: "3",
+      y: "8",
+      owner: null
+    }
+  ],
+  [
+    {
+      x: "4",
+      y: "0",
+      owner: null
+    },
+    {
+      x: "4",
+      y: "1",
+      owner: "red"
+    },
+    {
+      x: "4",
+      y: "2",
+      owner: "blue"
+    },
+    {
+      x: "4",
+      y: "3",
+      owner: "red"
+    },
+    {
+      x: "4",
+      y: "4",
+      owner: "blue"
+    },
+    {
+      x: "4",
+      y: "5",
+      owner: "blue"
+    },
+    {
+      x: "4",
+      y: "6",
+      owner: "red"
+    },
+    {
+      x: "4",
+      y: "7",
+      owner: "red"
+    },
+    {
+      x: "4",
+      y: "8",
+      owner: null
+    }
+  ],
+  [
+    {
+      x: "5",
+      y: "0",
+      owner: null
+    },
+    {
+      x: "5",
+      y: "1",
+      owner: "blue"
+    },
+    {
+      x: "5",
+      y: "2",
+      owner: "blue"
+    },
+    {
+      x: "5",
+      y: "3",
+      owner: null
+    },
+    {
+      x: "5",
+      y: "4",
+      owner: "red"
+    },
+    {
+      x: "5",
+      y: "5",
+      owner: "red"
+    },
+    {
+      x: "5",
+      y: "6",
+      owner: "red"
+    },
+    {
+      x: "5",
+      y: "7",
+      owner: "blue"
+    },
+    {
+      x: "5",
+      y: "8",
+      owner: "red"
+    }
+  ],
+  [
+    {
+      x: "6",
+      y: "0",
+      owner: "red"
+    },
+    {
+      x: "6",
+      y: "1",
+      owner: "blue"
+    },
+    {
+      x: "6",
+      y: "2",
+      owner: "blue"
+    },
+    {
+      x: "6",
+      y: "3",
+      owner: "blue"
+    },
+    {
+      x: "6",
+      y: "4",
+      owner: "red"
+    },
+    {
+      x: "6",
+      y: "5",
+      owner: "blue"
+    },
+    {
+      x: "6",
+      y: "6",
+      owner: "red"
+    },
+    {
+      x: "6",
+      y: "7",
+      owner: "blue"
+    },
+    {
+      x: "6",
+      y: "8",
+      owner: "blue"
+    }
+  ],
+  [
+    {
+      x: "7",
+      y: "0",
+      owner: null
+    },
+    {
+      x: "7",
+      y: "1",
+      owner: null
+    },
+    {
+      x: "7",
+      y: "2",
+      owner: "blue"
+    },
+    {
+      x: "7",
+      y: "3",
+      owner: "red"
+    },
+    {
+      x: "7",
+      y: "4",
+      owner: "blue"
+    },
+    {
+      x: "7",
+      y: "5",
+      owner: "red"
+    },
+    {
+      x: "7",
+      y: "6",
+      owner: "blue"
+    },
+    {
+      x: "7",
+      y: "7",
+      owner: "blue"
+    },
+    {
+      x: "7",
+      y: "8",
+      owner: "blue"
+    }
+  ],
+  [
+    {
+      x: "8",
+      y: "0",
+      owner: "blue"
+    },
+    {
+      x: "8",
+      y: "1",
+      owner: "blue"
+    },
+    {
+      x: "8",
+      y: "2",
+      owner: "blue"
+    },
+    {
+      x: "8",
+      y: "3",
+      owner: "blue"
+    },
+    {
+      x: "8",
+      y: "4",
+      owner: "red"
+    },
+    {
+      x: "8",
+      y: "5",
+      owner: "red"
+    },
+    {
+      x: "8",
+      y: "6",
+      owner: "blue"
+    },
+    {
+      x: "8",
+      y: "7",
+      owner: "red"
+    },
+    {
+      x: "8",
+      y: "8",
+      owner: "red"
+    }
+  ]
+];
+
 const testSliceX = () => {
   let winningX = null;
   let winningArray = [];
@@ -937,8 +1363,8 @@ const testSliceX = () => {
       return true;
     } else {
       setWinner = "Winner: " + winningColor;
-      console.log(setWinner, testBoardX[winningX].splice(winningY, 5));
-      testBoardX[winningX].splice(winningY, 5);
+      /*       console.log(setWinner, testBoardX[winningX].splice(winningY, 5));
+       */ testBoardX[winningX].splice(winningY, 5);
       return true;
     }
   }
@@ -1011,8 +1437,8 @@ const testSliceY = () => {
     if (fullWinning) {
       setGameWinner = `Game Winner: ${gameWinningColor}`;
       gameWinningArray.unshift(gameWinningArray[0] - 1); // console log only
-      console.log(gameWinningArray); // just for console log
-      console.log(setGameWinner);
+      /*       console.log(gameWinningArray); // just for console log
+       */ console.log(setGameWinner);
       return true;
     } else {
       let consoleSplice = []; // just for logging test
@@ -1021,16 +1447,50 @@ const testSliceY = () => {
       winningArray.forEach(x => {
         consoleSplice.push(testBoardY[x].splice(winningY, 1)); // get rid of the push in order to work
       });
-      console.log(setWinner, consoleSplice);
+      /* console.log(setWinner, consoleSplice); */
       return true;
     }
   }
 };
 
-it("checks for win on x", () => {
+const testSliceD = () => {
+  let inARow = 1;
+  let currentColor = null;
+  let winningColor = null;
+  let winning = testBoardD.some(element => {
+    element.some(subElement => {
+      if (subElement.owner !== null) {
+        currentColor = subElement.owner;
+        for (let i = 0; i < 5; i++) {
+          if (
+            testBoardD[+subElement.x + i][+subElement.y + i].owner ===
+            currentColor
+          ) {
+            winningColor = currentColor;
+            inARow = 5;
+          }
+        }
+      }
+      return inARow >= 5;
+    });
+    return inARow >= 5;
+  });
+  if (winning) {
+    console.log(`Winner: ${winningColor}`);
+    return true;
+  }
+};
+
+[0, 0], [1, 1], [2, 2], [3, 3], [4, 4];
+
+/* it("checks for win on x", () => {
   expect(testSliceX()).toEqual(true);
 });
 
 it("checks for win on y", () => {
   expect(testSliceY()).toEqual(true);
+}); */
+
+it("checks for win on diagonal", () => {
+  expect(testSliceD()).toEqual(true);
 });
