fa6e7c483e87c021e1fa1ef4828f8ce3dea8712d
JarrodM-M <85252030+JarrodM-M@users.noreply.github.com>
2022-08-24 21:44:54 -0230

    brought in win on y and full y

--- src/components/Win.js
+++ src/components/Win.js
@@ -74,3 +74,83 @@ const testFullX = board => {
     return true;
   }
 };
+
+const testSliceY = board => {
+  let winningArray = [];
+  let winningY = null;
+  let winningColor = null;
+  let setWinner = null;
+  let inARow = 1;
+  let lastOwner = null;
+  let lastX = null;
+  let newTestBoard = board.map((x, index) => board.map(y => y[index]));
+
+  let winning = newTestBoard.some(element => {
+    element.some(subElement => {
+      if (
+        subElement.owner !== null &&
+        lastOwner === subElement.owner &&
+        subElement.x - lastX == 1 &&
+        subElement.winOn !== "onY"
+      ) {
+        inARow += 1;
+        winningArray.push(subElement.x);
+        winningColor = subElement.owner;
+        winningY = subElement.y;
+      } else {
+        inARow = 1;
+        winningArray.splice(0, winningArray.length);
+      }
+
+      lastOwner = subElement.owner;
+      lastX = subElement.x;
+      return inARow >= 5;
+    });
+    return inARow >= 5;
+  });
+
+  if (winning) {
+    setWinner = "Winner: " + winningColor;
+    winningArray.unshift(winningArray[0] - 1);
+    winningArray.map(x => {
+      board[x][winningY].winOn = "onY";
+    });
+    console.log(setWinner + "testY");
+
+    return true;
+  }
+};
+
+const testFullY = board => {
+  let gameWinningColor = null;
+  let setGameWinner = null;
+  let lastX = null;
+  let lastOwner = null;
+  let inARow = 1;
+  let newTestBoard = board.map((x, index) => board.map(row => row[index]));
+
+  let fullWinning = newTestBoard.some(element => {
+    element.some(subElement => {
+      if (
+        subElement.owner !== null &&
+        lastOwner === subElement.owner &&
+        subElement.x - lastX == 1
+      ) {
+        inARow += 1;
+        gameWinningColor = subElement.owner;
+      } else {
+        inARow = 1;
+      }
+
+      lastOwner = subElement.owner;
+      lastX = subElement.x;
+      return inARow >= 9;
+    });
+    return inARow >= 9;
+  });
+  if (fullWinning) {
+    setGameWinner = `Game Winner: ${gameWinningColor}`;
+    console.log(setGameWinner + "full y");
+    return true;
+  }
+};
